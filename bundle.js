098146064f0dcf60f90b71f3a51df740d780e3f3
ann <anruolan_0608@126.com>
2019-07-20 16:04:56 +0800

    手写bundle文件，读取抽象语法树并解析

---
+++ bundle.js
@@ -0,0 +1,60 @@
+const fs = require('fs');
+const path = require('path');
+const parser = require('@babel/parser');
+const traverse = require('@babel/traverse').default; // 添加default转为es 6形式
+const babel  = require('@babel/core');
+
+const moduleAnalyser = ( filename ) => {
+    // 读取filename中的内容
+    const content = fs.readFileSync(filename, 'utf-8');
+    // 可通过paser将查看到的文件内容翻译为抽象语法树
+    const ast = parser.parse(content, {
+        sourceType:'module'
+    });
+    const dependencies = {};
+    // 对抽象语法树进行遍历，抽取所有import信息,对依赖文件进行分析，生成依赖关系
+    traverse(ast,{
+        ImportDeclaration({ node }){
+            const dirname = path.dirname(filename);
+            // 将相对路径改为绝对路径
+            const newFile = './' + path.join(dirname,node.source.value);
+            dependencies[node.source.value] = newFile;
+        }
+    });
+    // 将原始代码转化为浏览器可识别的代码，用到babel/core
+    console.log(dependencies);
+    const { code } = babel.transformFromAst(ast,null,{
+        presets:["@babel/preset-env"]
+    });
+    console.log(code);
+    return {
+        filename,
+        dependencies,
+        code
+    }
+}
+
+const makeDependenciesGraph = (entry) => {
+    const entryModule = moduleAnalyser(entry);
+    const graphArry = [entryModule];
+    for(let i=0;i<graphArry.length;i++){
+        const item = graphArry[i];
+        const { dependencies } = item;
+        for(let j in dependencies){
+            graphArry.push(moduleAnalyser(dependencies[j]));
+        }
+    }
+
+    const graph = {};
+    graphArry.forEach(item => {
+        graph[item.filename] = {
+            dependencies: item.dependencies,
+            code: item.code
+        }
+    })
+    return graph;
+}
+
+// const moduleInfo = moduleAnalyser('./src/index.js');
+const graphInfo = makeDependenciesGraph('./src/index.js');
+console.log(graphInfo);
